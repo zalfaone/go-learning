043eb90b7b0a40c84d61787bc73a218bef4fd41c
Rizal Fathur Rahman <54006854+zalfaone@users.noreply.github.com>
2023-06-18 17:05:08 +0700

    #07 Math Operation

---
+++ 07-math-operation.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	var b = 10
+	var c = a + b
+	fmt.Println(c)
+
+	var result = 10 * 10
+	fmt.Println(result)
+
+	var totalPrice = 20000
+	totalPrice += 20
+	fmt.Println(totalPrice)
+
+	totalPrice++
+	fmt.Println(totalPrice)
+
+	var negative = -100
+	var positive = +100
+	fmt.Println(negative)
+	fmt.Println(positive)
+
+}
