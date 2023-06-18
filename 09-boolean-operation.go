15ba8a811ea8bd058f02cd1c09f3ae00cc15bc64
Rizal Fathur Rahman <54006854+zalfaone@users.noreply.github.com>
2023-06-18 17:05:37 +0700

    #09 Boolean Operation

---
+++ 09-boolean-operation.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var ujian = 80
+	var absensi = 74
+
+	var lulusUjian = ujian >= 80
+	var lulusAbsensi = absensi >= 80
+	fmt.Println(lulusUjian)
+	fmt.Println(lulusAbsensi)
+
+	var lulus = lulusAbsensi && lulusUjian
+	fmt.Println(lulus)
+
+}
