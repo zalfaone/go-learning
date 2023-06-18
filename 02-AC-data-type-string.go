496f54c56a7b9be8d9e4487ff5e88c418dbb0ff4
Rizal Fathur Rahman <54006854+zalfaone@users.noreply.github.com>
2023-06-18 08:31:35 +0700

    #02-AA learn Data Type String

---
+++ 02-AC-data-type-string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Leon")
+	fmt.Println("Leon S Kennedy")
+	fmt.Println("Ada Wong")
+
+	fmt.Println(len("Leon"))
+	fmt.Println("Leon S Kennedy"[0])
+	fmt.Println("Ada Wong"[1])
+}
