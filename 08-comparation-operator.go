9b48bfe4882ea27037422d071eb4d05f8ade0320
Rizal Fathur Rahman <54006854+zalfaone@users.noreply.github.com>
2023-06-18 17:05:22 +0700

    #08 Comparation Operator

---
+++ 08-comparation-operator.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name1 = "Leon Kennedy"
+	var name2 = "Ada Wong"
+	fmt.Println(name1 > name2)
+
+	var a = 10
+	var b = 12
+	fmt.Println(a > b)
+	fmt.Println(a < b)
+	fmt.Println(a == b)
+	fmt.Println(a != b)
+
+}
