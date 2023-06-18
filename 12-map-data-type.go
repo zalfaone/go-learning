90788cc4eec969373267b5493d1ef0d84d744193
Rizal Fathur Rahman <54006854+zalfaone@users.noreply.github.com>
2023-06-18 17:06:27 +0700

    #12 Map Data Type

---
+++ 12-map-data-type.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	person := map[string]string{
+		"name":    "John",
+		"address": "San Francisco",
+	}
+	person["email"] = "john@gmail.com"
+
+	fmt.Println(person)
+	fmt.Println(person["name"])
+	fmt.Println(person["address"])
+
+	var book map[string]string = make(map[string]string)
+	book["title"] = "Harry Potter"
+	book["author"] = "JK Rowling"
+	book["ups"] = "Salah"
+
+	fmt.Println(book)
+	fmt.Println(len(book))
+
+	delete(book, "ups")
+
+	fmt.Println(book)
+	fmt.Println(len(book))
+
+}
