4c191759f85bd8c7f04c342841e828a093aabc5e
Rizal Fathur Rahman <54006854+zalfaone@users.noreply.github.com>
2023-06-24 10:24:29 +0700

    #14 Switch & Case Expression

---
+++ 14-switch-expression.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Albert Whesker"
+
+	switch name {
+	case "Leon":
+		fmt.Println("Hello Leon")
+	case "Hunnigan":
+		fmt.Println("Hello Hunnigan")
+	case "Claire":
+		fmt.Println("Hello Claire")
+	default:
+		fmt.Println("Lu sapa?")
+	}
+
+	subjects := "Math"
+	switch len(subjects) > 5 {
+	case true:
+		fmt.Println("Mapel terlalu panjang")
+	case false:
+		fmt.Println("Mapel pas")
+	}
+
+	weapons := [...]string{"M4", "AK47", "MP5", "UZI", "SCAR-L"}
+	switch {
+	case len(weapons) < 3:
+		fmt.Println("Please, bring more weapons bfeora war.")
+	case len(weapons) >= 5:
+		fmt.Println("Good, you are ready for war.")
+	default:
+		fmt.Println("You can start war now, or it's better to bring more weapons.")
+	}
+}
