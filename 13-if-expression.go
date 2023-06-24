03ad21aa19aaa8cf9e5d1bc0d0b3b474893469e8
Rizal Fathur Rahman <54006854+zalfaone@users.noreply.github.com>
2023-06-24 10:24:15 +0700

    #13 If Expression

---
+++ 13-if-expression.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Ada"
+
+	if name == "Leon" {
+		fmt.Println("Hello Leon")
+	} else if name == "Claire" {
+		fmt.Println("Hello Claire")
+	} else {
+		fmt.Println("Ini Bukan Leon")
+	}
+
+	if len(name) > 5 {
+		fmt.Println("Terlalu Panjang")
+	} else {
+		fmt.Println("Terlalu Panjang")
+	}
+
+}
