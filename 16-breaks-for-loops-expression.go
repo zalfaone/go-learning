c6bc2147c52e68bc2805cfe43536a9d7ddd50dae
Rizal Fathur Rahman <54006854+zalfaone@users.noreply.github.com>
2023-06-24 10:25:07 +0700

    #16 Breaks & Continue

---
+++ 16-breaks-for-loops-expression.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i <= 10; i++ {
+		if i == 7 {
+			break
+		}
+		fmt.Println("Perulangan ke", i)
+	}
+
+	weapons := map[string]string{
+		"meele":      "Knive",
+		"mid-range":  "Shotgun",
+		"long-range": "Sniper",
+	}
+
+	for i, value := range weapons {
+		if i == "mid-range" {
+			break
+		}
+		fmt.Println("Index", i, "=", value)
+	}
+
+	for i, value := range weapons {
+		if i == "mid-range" {
+			continue
+		}
+		fmt.Println("Index", i, "=", value)
+	}
+
+}
