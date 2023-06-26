b4fdb5aedfdf827de283e26fefbebbf951d9ecb1
Khoerul Rizal <rzl.trimelty@gmail.com>
2023-06-26 11:24:36 +0700

    fix: title

--- web-service-gin.go
+++ web-service-gin.go
@@ -15,7 +15,7 @@ type album struct {
 
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "2", Title: "Tom and Jerry", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
